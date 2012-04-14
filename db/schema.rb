653dc96139504df36b2dcc07a8fa79f3d0de499a
Amiruddin Nagri <amir.nagri+git@gmail.com>
2012-04-14 19:55:11 +0100

    amir - generated the new schema and create the needed tables after dropping old tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120414183953) do
+ActiveRecord::Schema.define(:version => 20120414185309) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -20,9 +20,12 @@ ActiveRecord::Schema.define(:version => 20120414183953) do
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.text     "locked_by"
+    t.string   "locked_by"
+    t.string   "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
 end
