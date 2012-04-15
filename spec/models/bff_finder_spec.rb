c8d46c9cb5f9631d7fbc4616808feb50d8deb172
Amiruddin Nagri <amir.nagri+git@gmail.com>
2012-04-15 13:42:30 +0100

    amir - changes in specs

--- spec/models/bff_finder_spec.rb
+++ spec/models/bff_finder_spec.rb
@@ -2,46 +2,48 @@ require 'spec_helper'
 require File.expand_path(File.dirname(__FILE__) + '/rollover_array')
 
 describe BffFinder do
-  it 'fetch comments for given friend and do processing' do
-    # given
-    rollover_statuses = prepare_test_feed # test feed with two user's comments
+  context 'non paginated comments' do
+    it 'fetch comments for given friend and do processing' do
+      # given
+      rollover_statuses = prepare_test_feed # test feed with two user's comments
 
-    @graph = mock('graph')
-    @graph.should_receive(:get_connections).with('12345', 'statuses', :limit => 100).and_return(rollover_statuses)
+      @api = mock('api')
+      @api.should_receive(:get_connections).with('12345', 'statuses', :limit => 100).and_return(rollover_statuses)
 
-    # when
-    @bff_finder = BffFinder.new(@graph, '12345')
-    @bff_finder.before(stub('job', 'id' => 1))
-    feed_result = @bff_finder.perform
+      # when
+      @bff_finder = BffFinder.new(@api, '12345')
+      @bff_finder.before(stub('job', 'id' => 1))
+      feed_result = @bff_finder.perform
 
-    # then
-    feed_result['job_id'].should == 1
-    feed_result['result'].should == "{\"Friend 1\":100,\"Friend 2\":50,\"Friend 3\":50}"
-  end
+      # then
+      feed_result['job_id'].should == 1
+      feed_result['result'].should == "{\"Friend 1\":100,\"Friend 2\":50,\"Friend 3\":50}"
+    end
 
-  def prepare_test_feed
-    status_1 = {
-        "comments"=> {
-            "data"=>
-                [{"from"=>{"name"=>"Friend 1"}, "message"=>"no"}, {"from"=>{"name"=>"Friend 3", }, "message"=>"yes"}]
-        },
-        "from"=>{
-            "name"=>"My Friend",
-        },
-        "message"=>"foobar",
-    }
+    def prepare_test_feed
+      status_1 = {
+          "comments"=> {
+              "data"=>
+                  [{"from"=>{"name"=>"Friend 1"}, "message"=>"no"}, {"from"=>{"name"=>"Friend 3", }, "message"=>"yes"}]
+          },
+          "from"=>{
+              "name"=>"My Friend",
+          },
+          "message"=>"foobar",
+      }
 
-    status_2 = {
-        "comments"=> {
-            "data"=> [{"from"=>{"name"=>"Friend 2"}, "message"=>"no"}, {"from"=>{"name"=>"Friend 1", }, "message"=>"yes"}]
-        },
-        "from"=>{
-            "name"=>"My Friend",
-        },
-        "message"=>"barfoo",
-    }
+      status_2 = {
+          "comments"=> {
+              "data"=> [{"from"=>{"name"=>"Friend 2"}, "message"=>"no"}, {"from"=>{"name"=>"Friend 1", }, "message"=>"yes"}]
+          },
+          "from"=>{
+              "name"=>"My Friend",
+          },
+          "message"=>"barfoo",
+      }
 
-    rollover_feed = RolloverArray.new([status_1, status_2], 100)
-    rollover_feed
+      rollover_feed = RolloverArray.new([status_1, status_2], 100)
+      rollover_feed
+    end
   end
 end
